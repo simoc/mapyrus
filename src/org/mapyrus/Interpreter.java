e512eebdd3a3763bcb6242d6fe13fa8ff6ca6559
simonc <devnull@localhost>
2002-12-08 10:46:45 +0000

    Initial revision

---
+++ src/org/mapyrus/Interpreter.java
@@ -0,0 +1,227 @@
+/**
+ * Language interpreter.  Parse and executes commands read from file, or
+ * typed by user.
+ */
+
+/*
+ * $Id$
+ */
+
+import java.io.*;
+import java.lang.String;
+import java.util.Vector;
+
+public class Interpreter extends Thread
+{
+	/*
+	 * States during parsing an expression.
+	 */
+	private static final int AT_START = 1;
+	private static final int AT_START_ARGS = 2;
+	private static final int IN_ARGS = 3;
+	private static final int IN_COMMENT = 4;
+
+	Preprocessor mPre;
+
+	private Statement parseStatement() throws GfException, IOException
+	{
+		int c;
+		int state = AT_START;
+		int type;
+		StringBuffer keyword = new StringBuffer();
+		Vector expressions = new Vector();
+		ArithmeticExpression expr;
+		Statement retval = null;
+		boolean isAssign = false;
+
+		c = mPre.read();
+		while (true)
+		{
+			if (c == -1)
+			{
+				/*
+				 * Reached EOF.
+				 */
+				break;
+			}
+			else if (c == '#')
+			{
+				state = IN_COMMENT;
+				c = mPre.read();
+			}
+			else
+			{
+				type = Character.getType((char)c);
+				if (c == Character.LINE_SEPARATOR)
+				{
+					if (state == AT_START_ARGS || state == IN_ARGS)
+					{
+						/*
+						 * End of line signals
+						 * end of statement.
+						 */
+						break;
+					}
+					else
+					{
+						/*
+						 * We did not even begin reading
+						 * a statement so expect to
+						 * read one on the next line.
+						 */
+						state = AT_START;
+					}
+					c = mPre.read();
+				}
+				else if (Character.isWhitespace((char)c))
+				{
+					/*
+					 * Skip whitespace
+					 */
+					c = mPre.read();
+				}
+				else if (state == IN_COMMENT)
+				{
+					/*
+					 * Do nothing -- we are reading a comment.
+					 */
+					c = mPre.read();
+				}
+				else if (state == AT_START)
+				{
+					/*
+					 * A statement begins with a keyword
+					 * which must begin with a letter.
+					 */
+					if (!Character.isLetter((char)c))
+					{
+						throw new GfException("Invalid statement at " + mPre.getCurrentFilenameAndLine());
+					}
+
+					/*
+					 * Read in whole keyword.
+					 */
+					do
+					{
+						keyword.append((char)c);
+						c = mPre.read();
+					}
+					while (Character.isLetterOrDigit((char)c));
+					state = AT_START_ARGS;
+				}
+				else if (state == AT_START_ARGS)
+				{
+					/*
+					 * Is this an assignment statement or
+					 * some other kind of statement?
+					 */
+					isAssign = (c == '=');
+					if (isAssign)
+					{
+						c = mPre.read();
+					}
+					state = IN_ARGS;
+				}
+				else if (state == IN_ARGS)
+				{
+					/*
+					 * Parse an expression.
+					 */
+					expr = new ArithmeticExpression(mPre);
+					expressions.add(expr);
+
+					/*
+					 * Gobble up any ',' separator too.
+					 */
+					c = mPre.read();
+					if (c == '\n')
+					{
+						mPre.unread(c);
+					}
+					else if (c != ',')
+					{
+						throw new GfException("Expecting ',' at " + mPre.getCurrentFilenameAndLine());
+					}
+				}
+			}
+		}
+
+		/*
+		 * Build a statement structure for what we just parsed.
+		 */
+		if (isAssign)
+		{
+			if (expressions.size() > 1)
+			{
+				throw new GfException("Too many expressions in assignment at " + mPre.getCurrentFilenameAndLine());
+			}
+			else if (expressions.size() == 0)
+			{
+				throw new GfException("No expression in assignment at " + mPre.getCurrentFilenameAndLine());
+			}
+			retval = new Statement(keyword.toString(),
+				(ArithmeticExpression)expressions.elementAt(0));
+		}
+		else
+		{
+			int statementType;
+			ArithmeticExpression []a;
+
+			statementType = Statement.getStatementType(keyword.toString());
+			a = (ArithmeticExpression [])expressions.toArray();
+			if (statementType < 0)
+			{
+				throw new GfException("Keyword " + keyword + " not recognized " + mPre.getCurrentFilenameAndLine());
+			}
+			retval = new Statement(statementType, a);
+		}
+		return(retval);
+	}
+
+	/**
+	 * Begin interpretation.
+	 */
+	public void run()
+	{
+		Statement st;
+
+		try
+		{
+			/*
+			 * Keep parsing until we read EOF.
+			 */
+			while ((st = parseStatement()) != null)
+			{
+				System.out.println("Parsed a statement");
+				System.out.println(st.getType());
+			}
+		}
+		catch (GfException e)
+		{
+		}
+		catch (IOException e)
+		{
+		}
+	}
+
+	/**
+	 * Create new language interpreter from a file.
+	 * @param filename is file to read from.
+	 */
+	public Interpreter(String filename) throws GfException, FileNotFoundException
+	{
+		super();
+		mPre = new Preprocessor(filename);
+	}
+
+	/**
+	 * Create new language interpreter.
+	 * @param in is opened Reader to read from.
+	 */
+	public Interpreter(Reader in) throws GfException
+	{
+		super();
+		mPre = new Preprocessor(in);
+	}
+}
+
