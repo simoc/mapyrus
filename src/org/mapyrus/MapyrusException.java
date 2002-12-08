8eb49b66bb5595199867077966f9cca66d348ff0
simonc <devnull@localhost>
2002-12-08 15:13:36 +0000

    Initial revision

---
+++ src/org/mapyrus/MapyrusException.java
@@ -0,0 +1,21 @@
+/**
+ * This class indicates exceptions in the Mapyrus software that can be caught.
+ */
+ 
+/*
+ * @(#) $Id$
+ */
+import java.lang.String;
+
+public class MapyrusException extends Exception
+{
+	public MapyrusException()
+	{
+		super();
+	}
+
+	public MapyrusException(String s)
+	{
+		super(s);
+	}
+}
