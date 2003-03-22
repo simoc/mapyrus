c0d3768a74f68973ca35716621f8739dd0060422
simonc <devnull@localhost>
2003-03-22 17:54:35 +0000

    Take data types from Argument class instead.

--- src/org/mapyrus/Row.java
+++ src/org/mapyrus/Row.java
@@ -1,11 +1,11 @@
+/*
+ * $Id$
+ */
+ 
 /**
  * A row read from a geographic dataset containing one geometrical object plus
  * its attributes.
  * Or as the OGIS people would say, a "simple feature".
- */
-
-/*
- * $Id$
  */
 package net.sourceforge.mapyrus;
 
@@ -13,10 +13,4 @@ import java.util.Vector;
 
 public class Row extends Vector 
 {
-	/*
-	 * Types of attributes for a feature.
-	 */
-	public static final int NUMBER = 1;
-	public static final int STRING = 2;
-	public static final int GEOMETRY = 3;
 }
