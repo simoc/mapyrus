39e660409c728d2357d40fce5cdbd51724b9360b
simonc <devnull@localhost>
2003-05-29 07:03:51 +0000

    Constants that are required in several isolated classes.

---
+++ src/org/mapyrus/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * @(#) $Id$
+ */
+package au.id.chenery.mapyrus;
+
+/**
+ * Globally useful constants including fixed distance measurements.
+ */
+public class Constants
+{
+	/*
+	 * Number of points and millimetres per inch.
+	 */
+	public static final int POINTS_PER_INCH = 72;
+	public static final double MM_PER_INCH = 25.4;
+}
