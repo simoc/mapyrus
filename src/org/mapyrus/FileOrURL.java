9b5e2ee12f3d394b96a2daa403eb3b142557491a
simonc <devnull@localhost>
2003-09-06 18:10:32 +0000

    Wrapper, providing same interface to File and URL objects.

---
+++ src/org/mapyrus/FileOrURL.java
@@ -0,0 +1,206 @@
+/*
+ * This file is part of Mapyrus, software for plotting maps.
+ * Copyright (C) 2003 Simon Chenery.
+ *
+ * Mapyrus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapyrus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mapyrus; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * @(#) $Id$
+ */
+package au.id.chenery.mapyrus;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Wrapper around java.io.FileInputStream and java.net.URL classes to present
+ * uniform interface for opening, closing and getting InputStream to read from.
+ */
+public class FileOrURL
+{
+	private String mName;
+	private URL mURL;
+	private BufferedInputStream mInputStream;
+	private LineNumberReader mReader;
+	private boolean mIsURL;
+
+	/**
+	 * Open file or URL.
+	 * @param name name of file or URL to open.
+	 * @throws IOException
+	 */
+	public FileOrURL(String name) throws FileNotFoundException, MapyrusException
+	{
+		InputStream in;
+		try
+		{
+			/*
+			 * First try opening as an URL.
+			 */
+			mURL = new URL(name);
+			try
+			{
+				in = mURL.openStream();
+			}
+			catch (IOException e)
+			{
+				/*
+				 * The IOException error message is not very helpful.  Throw our own exception.
+				 */
+				throw new MapyrusException(MapyrusMessages.get(MapyrusMessages.CANNOT_OPEN_URL) + ": " +
+					mURL.toString() + Constants.LINE_SEPARATOR + e.getMessage());
+			}
+			mInputStream = new BufferedInputStream(in);
+			mReader = new LineNumberReader(new InputStreamReader(in));
+			mIsURL = true;
+		}
+		catch (MalformedURLException e)
+		{
+			/*
+			 * It is not a valid URL, try opening it as a plain file instead.
+			 */
+			FileInputStream f = new FileInputStream(name);
+			mInputStream = new BufferedInputStream(f);
+			mReader = new LineNumberReader(new InputStreamReader(f));
+			mIsURL = false;
+		}
+		mName = name;
+	}
+
+	/**
+	 * Open file or URL.
+	 * @param name filename or URL to open.
+	 * @param contextURL parent URL from which this URL is being loaded, or null
+	 * if we are not loading from within the context of another URL.
+	 */
+	public FileOrURL(String name, FileOrURL contextURL)
+		throws FileNotFoundException, MapyrusException
+	{
+		InputStream in;
+
+		try
+		{
+			if (contextURL != null)
+				mURL = new URL(contextURL.mURL, name);
+			else
+				mURL = new URL(name);
+
+			try
+			{
+				in = mURL.openStream();
+			}
+			catch (IOException e)
+			{
+				/*
+				 * The IOException error message is not very helpful.  Throw our own exception.
+				 */
+				throw new MapyrusException(MapyrusMessages.get(MapyrusMessages.CANNOT_OPEN_URL) + ": " +
+					mURL.toString() + Constants.LINE_SEPARATOR + e.getMessage());
+			}
+			mInputStream = new BufferedInputStream(in);
+			mReader = new LineNumberReader(new InputStreamReader(in));
+			mIsURL = true;
+			mName = mURL.toString();
+		}
+		catch (MalformedURLException e)
+		{
+			mURL = null;
+		}
+
+		if (mURL == null)
+		{
+			/*
+			 * It is not a valid URL, try opening it as a plain file instead.
+			 */
+			FileInputStream f = new FileInputStream(name);
+			mInputStream = new BufferedInputStream(f);
+			mReader = new LineNumberReader(new InputStreamReader(f));
+			mIsURL = false;
+			mName = name;
+		}
+	}
+
+	/**
+	 * Create object from file that is already open.
+	 * @param reader file opened for reading.
+	 * @param name name of file.
+	 */
+	public FileOrURL(Reader reader, String name)
+	{
+		mIsURL = false;
+		mName = name;
+		mReader = new LineNumberReader(reader);
+		
+		/*
+		 * Text file reader cannot be converted for reading as a java.io.InputStream.
+		 */
+		mInputStream = null;
+		
+	}
+
+	/**
+	 * Returns true if reading a URL, not a plain file.
+	 * @return true if reading URL, false if reading a plain file.
+	 */	
+	public boolean isURL()
+	{
+		return(mIsURL);
+	}
+
+	/**
+	 * Returns content type of URL being read.
+	 * @return content type for URL being read.
+	 */
+	public String getURLContentType() throws IOException
+	{	
+		return(mURL.openConnection().getContentType());
+	}
+
+	/**
+	 * Returns java.io.BufferedInputStream for reading from this file or URL.
+	 * @return stream to read from.
+	 */
+	public BufferedInputStream getInputStream()
+	{
+		return(mInputStream);
+	}
+
+	/**
+	 * Returns java.io.LineNumberReader for reading from this file or URL.
+	 * @return reader to read from.
+	 */
+	public LineNumberReader getReader()
+	{
+		return(mReader);
+	}
+	
+	/**
+	 * Returns name of file or URL we are reading from.
+	 * @return filename or URL.
+	 */
+	public String toString()
+	{
+		return(mName);
+	}
+}
