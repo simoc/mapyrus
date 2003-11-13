c615976fe708afe909343ae39975122816c2ae6a
simonc <devnull@localhost>
2003-11-13 06:12:35 +0000

    Remove unused variable.

--- src/org/mapyrus/dataset/TextfileDataset.java
+++ src/org/mapyrus/dataset/TextfileDataset.java
@@ -23,7 +23,6 @@
 package au.id.chenery.mapyrus.dataset;
 
 import java.awt.geom.Rectangle2D;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.FileNotFoundException;
@@ -101,7 +100,6 @@ public class TextfileDataset implements GeographicDataset
 	{
 		StringTokenizer st;
 		String token;
-		BufferedReader bufferedReader;
 
 		/*
 		 * Check if we should start a program and read its output instead
