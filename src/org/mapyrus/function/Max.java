5172a729c2dfaec111e57857873719e42dac2efb
schenery <devnull@localhost>
2004-03-21 20:25:37 +0000

    Split functions out from Expression.java into separate classes.

---
+++ src/org/mapyrus/function/Max.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of Mapyrus, software for plotting maps.
+ * Copyright (C) 2003 Simon Chenery.
+ *
+ * Mapyrus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapyrus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mapyrus; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * @(#) $Id$
+ */
+package org.mapyrus.function;
+
+import org.mapyrus.Argument;
+import org.mapyrus.ContextStack;
+import org.mapyrus.MapyrusException;
+
+/**
+ * Function returning maximum of two values.
+ * For example, max(1, 3) = 3.
+ */
+public class Max extends Function
+{
+	/**
+	 * @see org.mapyrus.function.Function#evaluate(org.mapyrus.ContextStack, org.mapyrus.Argument, org.mapyrus.Argument)
+	 */
+	public Argument evaluate(ContextStack context, Argument arg1, Argument arg2)
+		throws MapyrusException
+	{
+		double d1 = arg1.getNumericValue();
+		double d2 = arg2.getNumericValue();
+		Argument retval = (d1 > d2) ? arg1 : arg2;
+		return(retval);
+	}
+
+	/**
+	 * @see org.mapyrus.function.Function#getMaxArgumentCount()
+	 */
+	public int getMaxArgumentCount()
+	{
+		return(2);
+	}
+
+	/**
+	 * @see org.mapyrus.function.Function#getMinArgumentCount()
+	 */
+	public int getMinArgumentCount()
+	{
+		return(2);
+	}
+
+	/**
+	 * @see org.mapyrus.function.Function#getName()
+	 */
+	public String getName()
+	{
+		return("max");
+	}
+}
