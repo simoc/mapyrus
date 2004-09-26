65ed74c8c93023ec20af03ad900d730cb6247148
schenery <devnull@localhost>
2004-09-26 20:47:22 +0000

    Add message invalidoption.

--- src/org/mapyrus/MapyrusMessages.java
+++ src/org/mapyrus/MapyrusMessages.java
@@ -89,6 +89,7 @@ public class MapyrusMessages
 	public static final String INVALID_NUMBER = "invalidnumber";
 	public static final String INVALID_OGC_WKB = "invalidogcwkb";
 	public static final String INVALID_OGC_WKT = "invalidogcwkt";
+	public static final String INVALID_OPTION = "invalidoption";
 	public static final String INVALID_OUTPUT = "invalidoutput";
 	public static final String INVALID_PAGE = "invalidpage";
 	public static final String INVALID_PAGE_SIZE = "invalidpagesize";
