880524d962be289263dec285aca8f28593cd78f5
schenery <devnull@localhost>
2004-09-28 04:38:04 +0000

    Add invalidpagerange message.

--- src/org/mapyrus/MapyrusMessages.java
+++ src/org/mapyrus/MapyrusMessages.java
@@ -92,6 +92,7 @@ public class MapyrusMessages
 	public static final String INVALID_OPTION = "invalidoption";
 	public static final String INVALID_OUTPUT = "invalidoutput";
 	public static final String INVALID_PAGE = "invalidpage";
+	public static final String INVALID_PAGE_RANGE = "invalidpagerange";
 	public static final String INVALID_PAGE_SIZE = "invalidpagesize";
 	public static final String INVALID_PAGE_RESOLUTION = "invalidpageresolution";
 	public static final String INVALID_PATH_OFFSET = "invalidpathoffset";
