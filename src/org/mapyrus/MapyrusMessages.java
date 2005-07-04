790ed5255b5ceba26480960d927c2ed5f9b7f161
schenery <devnull@localhost>
2005-07-04 20:07:14 +0000

    Add invalidspiral.

--- src/org/mapyrus/MapyrusMessages.java
+++ src/org/mapyrus/MapyrusMessages.java
@@ -119,6 +119,7 @@ public class MapyrusMessages
 	public static final String INVALID_REGEX = "invalidregex";
 	public static final String INVALID_ROTATION = "invalidrotation";
 	public static final String INVALID_SCALING = "invalidscaling";
+	public static final String INVALID_SPIRAL = "invalidspiral";
 	public static final String INVALID_STAR = "invalidstar";
 	public static final String INVALID_TABLE = "invalidtable";
 	public static final String INVALID_TRANSFORM = "invalidtransform";
