8ebb4bbcf0e189ed95016aa75ab3210612e0af38
Simon Chenery <simoc@users.sourceforge.net>
2006-01-01 18:12:21 +0000

    Add year 2006 to copyright messages.

--- src/org/mapyrus/io/WildcardFile.java
+++ src/org/mapyrus/io/WildcardFile.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Mapyrus, software for plotting maps.
- * Copyright (C) 2003, 2004, 2005 Simon Chenery.
+ * Copyright (C) 2003, 2004, 2005, 2006 Simon Chenery.
  *
  * Mapyrus is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
