9ee2086337b16b81c29cc5903369666863313fc2
Simon Chenery <simoc@users.sourceforge.net>
2006-01-13 21:13:34 +0000

    New function to add array(s) of numbers.

---
+++ src/org/mapyrus/function/Sum.java
@@ -0,0 +1,88 @@
+/*
+ * This file is part of Mapyrus, software for plotting maps.
+ * Copyright (C) 2003, 2004, 2005, 2006 Simon Chenery.
+ *
+ * Mapyrus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapyrus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mapyrus; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * @(#) $Id$
+ */
+package org.mapyrus.function;
+
+import java.util.ArrayList;
+
+import org.mapyrus.Argument;
+import org.mapyrus.ContextStack;
+import org.mapyrus.MapyrusException;
+
+/**
+ * Function returning sum of all values in one or more arrays.
+ * For example, if a[1] = 10, a[2] = 20 then sum(a) = 30.
+ */
+public class Sum implements Function
+{
+	/**
+	 * @see org.mapyrus.function.Function#evaluate(org.mapyrus.ContextStack, ArrayList)
+	 */
+	public Argument evaluate(ContextStack context, ArrayList args)
+		throws MapyrusException
+	{
+		double total = 0;
+
+		/*
+		 * Add all numbers and arrays of numbers together.
+		 */
+		for (int i = 0; i < args.size(); i++)
+		{
+			Argument arg = (Argument)args.get(i);
+			if (arg.getType() == Argument.HASHMAP)
+			{
+				Object []keys = arg.getHashMapKeys();
+				for (int j = 0; j < keys.length; j++)
+					total += arg.getHashMapEntry(keys[j].toString()).getNumericValue();
+			}
+			else
+			{
+				total += arg.getNumericValue();
+			}
+		}
+		return(new Argument(total));
+	}
+
+	/**
+	 * @see org.mapyrus.function.Function#getMaxArgumentCount()
+	 */
+	public int getMaxArgumentCount()
+	{
+		return(Integer.MAX_VALUE);
+	}
+
+	/**
+	 * @see org.mapyrus.function.Function#getMinArgumentCount()
+	 */
+	public int getMinArgumentCount()
+	{
+		return(1);
+	}
+
+	/**
+	 * @see org.mapyrus.function.Function#getName()
+	 */
+	public String getName()
+	{
+		return("sum");
+	}
+}
