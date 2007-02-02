0aa116fa59aa45541f95662e338667541ef200e9
Simon Chenery <simoc@users.sourceforge.net>
2007-02-02 20:48:49 +0000

    Add PDF command.

--- src/org/mapyrus/Statement.java
+++ src/org/mapyrus/Statement.java
@@ -92,23 +92,24 @@ public class Statement
 	public static final int EPS = 57;
 	public static final int SVG = 58;
 	public static final int SVGCODE = 59;
-	public static final int SCALE = 60;
-	public static final int ROTATE = 61;
-	public static final int WORLDS = 62;
-	public static final int PROJECT = 63;
-	public static final int DATASET = 64;
-	public static final int FETCH = 65;
-	public static final int NEWPAGE = 66;
-	public static final int ENDPAGE = 67;
-	public static final int SETOUTPUT = 68;
-	public static final int PRINT = 69;
-	public static final int LOCAL = 70;
-	public static final int LET = 71;
-	public static final int EVAL = 72;
-	public static final int KEY = 73;
-	public static final int LEGEND = 74;
-	public static final int MIMETYPE = 75;
-	public static final int HTTPRESPONSE = 76;
+	public static final int PDF = 60;
+	public static final int SCALE = 61;
+	public static final int ROTATE = 62;
+	public static final int WORLDS = 63;
+	public static final int PROJECT = 64;
+	public static final int DATASET = 65;
+	public static final int FETCH = 66;
+	public static final int NEWPAGE = 67;
+	public static final int ENDPAGE = 68;
+	public static final int SETOUTPUT = 69;
+	public static final int PRINT = 70;
+	public static final int LOCAL = 71;
+	public static final int LET = 72;
+	public static final int EVAL = 73;
+	public static final int KEY = 74;
+	public static final int LEGEND = 75;
+	public static final int MIMETYPE = 76;
+	public static final int HTTPRESPONSE = 77;
 
 	/*
 	 * Statement type for call and return to/from user defined procedure block.
@@ -212,6 +213,7 @@ public class Statement
 		mStatementTypeLookup.put("eps", new Integer(EPS));
 		mStatementTypeLookup.put("svg", new Integer(SVG));
 		mStatementTypeLookup.put("svgcode", new Integer(SVGCODE));
+		mStatementTypeLookup.put("pdf", new Integer(PDF));
 		mStatementTypeLookup.put("scale", new Integer(SCALE));
 		mStatementTypeLookup.put("rotate", new Integer(ROTATE));
 		mStatementTypeLookup.put("worlds", new Integer(WORLDS));
