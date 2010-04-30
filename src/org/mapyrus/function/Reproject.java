f66accb93b14c9f2d5601fac7ec91cee362197b0
Simon Chenery <simoc@users.sourceforge.net>
2010-04-30 11:35:34 +0000

    Accept PROJ.4 projection parameters instead of projection name. Remove points from geometry that cannot be reprojected.

--- src/org/mapyrus/function/Reproject.java
+++ src/org/mapyrus/function/Reproject.java
@@ -1,3 +1,25 @@
+/*
+ * This file is part of Mapyrus, software for plotting maps.
+ * Copyright (C) 2003 - 2010 Simon Chenery.
+ *
+ * Mapyrus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapyrus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mapyrus; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * @(#) $Id$
+ */
 package org.mapyrus.function;
 
 import java.awt.geom.Point2D;
@@ -11,6 +33,7 @@ import org.mapyrus.MapyrusMessages;
 
 import com.jhlabs.map.proj.Projection;
 import com.jhlabs.map.proj.ProjectionFactory;
+import com.jhlabs.map.proj.ProjectionException;
 
 /**
  * Reprojects a point from one projection to another.
@@ -18,7 +41,26 @@ import com.jhlabs.map.proj.ProjectionFactory;
  */
 public class Reproject implements Function
 {
-	private Hashtable<String, Projection> m_projectionCache = new Hashtable<String, Projection>();
+	private Hashtable<String, Projection> m_projectionCache;
+
+	public Reproject()
+	{
+		m_projectionCache = new Hashtable<String, Projection>();
+
+		/*
+		 * Force a projection to be loaded so we get a class
+		 * not found exception at startup, not halfway through
+		 * running some commands.
+		 */
+		try
+		{
+			String []parameters = new String[]{"+proj=latlong"};
+			ProjectionFactory.fromPROJ4Specification(parameters);
+		}
+		catch (ProjectionException e)
+		{
+		}
+	}
 
 	/**
 	 * @see org.mapyrus.function.Function#evaluate(org.mapyrus.ContextStack, ArrayList)
@@ -44,9 +86,10 @@ public class Reproject implements Function
 
 			reproject(srcProj, destProj, pt);
 
+			int nCoords = (Double.isNaN(pt.x) || Double.isNaN(pt.y)) ? 0 : 1;
 			double []coords = new double[5];
 			coords[0] = Argument.GEOMETRY_POINT;
-			coords[1] = 1;
+			coords[1] = nCoords;
 			coords[2] = Argument.MOVETO;
 			coords[3] = pt.x;
 			coords[4] = pt.y;
@@ -81,7 +124,7 @@ public class Reproject implements Function
 		double []coords, int coordIndex) throws MapyrusException
 	{
 		int geometryType = (int)coords[coordIndex++];
-		int nCoords;
+		int nCoords, nCoordsIndex;
 		Point2D.Double pt = new Point2D.Double();
 
 		switch (geometryType)
@@ -89,15 +132,37 @@ public class Reproject implements Function
 			case Argument.GEOMETRY_POINT:
 			case Argument.GEOMETRY_LINESTRING:
 			case Argument.GEOMETRY_POLYGON:
-				nCoords = (int)coords[coordIndex++];
+				nCoordsIndex = coordIndex++;
+				nCoords = (int)coords[nCoordsIndex];
 				for (int i = 0; i < nCoords; i++)
 				{
 					pt.x = coords[coordIndex + 1];
 					pt.y = coords[coordIndex + 2];
 					reproject(srcProj, destProj, pt);
-					coords[coordIndex + 1] = pt.x;
-					coords[coordIndex + 2] = pt.y;
-					coordIndex += 3;
+					if (Double.isNaN(pt.x) || Double.isNaN(pt.y))
+					{
+						/*
+						 * This pair of coordinates cannot be reprojected.
+						 * Remove the pair of coordinates from the list,
+						 * shuffling later coordinates left to fill their place.
+						 */
+						coords[nCoordsIndex]--;
+
+						int op = (int)coords[coordIndex];
+						System.arraycopy(coords, coordIndex + 3, coords, coordIndex, coords.length - coordIndex - 3);
+
+						/*
+						 * Ensure we do not lose any MOVE at the start of the geometry.
+						 */
+						if (op == Argument.MOVETO && coords[nCoordsIndex] > 0)
+							coords[coordIndex] = op;
+					}
+					else
+					{
+						coords[coordIndex + 1] = pt.x;
+						coords[coordIndex + 2] = pt.y;
+						coordIndex += 3;
+					}
 				}
 				break;
 			case Argument.GEOMETRY_MULTIPOINT:
@@ -148,9 +213,25 @@ public class Reproject implements Function
 	private Projection getProjection(String projectionName) throws MapyrusException
 	{
 		Projection retval = m_projectionCache.get(projectionName);
+
 		if (retval == null)
 		{
-			retval = ProjectionFactory.getNamedPROJ4CoordinateSystem(projectionName);
+			/*
+			 * Is projection a name or list of
+			 * projection parameters?
+			 */
+			String []parameters = projectionName.trim().split("\\s+");
+			try
+			{
+				if (parameters.length > 0 && parameters[0].startsWith("+"))
+					retval = ProjectionFactory.fromPROJ4Specification(parameters);
+				else
+					retval = ProjectionFactory.getNamedPROJ4CoordinateSystem(projectionName);
+			}
+			catch (ProjectionException e)
+			{
+				throw new MapyrusException(e.getMessage() + ": " + projectionName);
+			}
 			if (retval != null)
 				m_projectionCache.put(projectionName, retval);
 		}
