1f269dbdb4f9f70f1e4a903fe31a6fdd11bac0e9
Simon Chenery <simoc@users.sourceforge.net>
2010-06-26 15:11:48 +0000

    Replace static methods with object methods so each interpreter can have it's own independent throttle.

--- src/org/mapyrus/Throttle.java
+++ src/org/mapyrus/Throttle.java
@@ -27,22 +27,36 @@ package org.mapyrus;
  */
 public class Throttle
 {
-	static private long m_startTime;
-	static
-	{
-		m_startTime = System.currentTimeMillis();
-	};
+	private long m_startTime;
 
 	/*
 	 * Number of milliseconds to run in each second.
 	 */
-	static private long m_millisToUse = 1000;
+	private long m_millisToUse = 1000;
+
+	/*
+	 * Total number of milliseconds that thread can run for.
+	 */
+	private long m_timeLimit = -1;
+
+	public Throttle()
+	{
+		m_startTime = System.currentTimeMillis();
+	}
+
+	/**
+	 * Set thread start time to current time again.
+	 */
+	public void restart()
+	{
+		m_startTime = System.currentTimeMillis();
+	}
 
 	/**
 	 * Set maximum percentage of CPU to use.
 	 * @param percent maximum percentage to use.
 	 */
-	public static void setMaxLoad(int percent)
+	public void setMaxLoad(int percent)
 	{
 		if (percent < 5)
 			percent = 5;
@@ -52,18 +66,43 @@ public class Throttle
 	}
 
 	/**
+	 * Set maximum time that thread may run for.
+	 * @param millis number of milliseconds thread may run for.
+	 */
+	public void setMaxTime(long millis)
+	{
+		m_timeLimit = millis;
+	}
+
+	/**
+	 * Get maximum time that a thread may run for.
+	 * @return maximum time in milliseconds, or -1 if no limit.
+	 */
+	public long getMaxTime()
+	{
+		return(m_timeLimit);
+	}
+
+	/**
 	 * Sleep to limit CPU usage.
 	 * Should be called several times a second so sleeping
 	 * can be done accurately.
 	 */
-	public static void sleep() throws MapyrusException
+	public void sleep() throws MapyrusException
 	{
-		if (m_millisToUse != 1000)
+		if (m_millisToUse != 1000 || m_timeLimit > 0)
 		{
 			long now = System.currentTimeMillis();
 			long elapsed = now - m_startTime;
 			long millis = (elapsed % 1000);
-			if (millis > m_millisToUse)
+			if (m_timeLimit > 0 && elapsed > m_timeLimit)
+			{
+				/*
+				 * Thread has run for too long.  Interrupt it.
+				 */
+				throw new MapyrusException(MapyrusMessages.get(MapyrusMessages.INTERRUPTED));
+			}
+			else if (millis > m_millisToUse)
 			{
 				try
 				{
@@ -79,5 +118,16 @@ public class Throttle
 			}
 		}
 	}
-}
 
+	/**
+	 * Make a clone of this throttle.
+	 * @return clone of this throttle.
+	 */
+	public Throttle clone()
+	{
+		Throttle retval = new Throttle();
+		retval.m_timeLimit = m_timeLimit;
+		retval.m_millisToUse = m_millisToUse;
+		return(retval);
+	}
+}
