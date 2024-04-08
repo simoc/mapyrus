6f552090592e2f92ee5b938c5a90fca248bf7dd3
Simon Chenery <simon_chenery@yahoo.com>
2024-04-08 21:40:46 +0200

    Setup skeleton unit test class TestASCII85Writer

---
+++ src/test/java/org/mapyrus/io/TestASCII85Writer.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Mapyrus, software for plotting maps.
+ * Copyright (C) 2003 - 2024 Simon Chenery.
+ *
+ * Mapyrus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapyrus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mapyrus; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.mapyrus.io;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class TestASCII85Writer
+{
+	@Test
+	public void testWriteZeroBytes() throws IOException
+	{
+		StringWriter writer = new StringWriter();
+		ASCII85Writer ascii85 = new ASCII85Writer(writer, false);
+		ascii85.close();
+	}
+}
